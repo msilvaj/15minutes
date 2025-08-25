7046d3bcec8740ef4d40712924a6b9efb653d57d
Miqueias da Silva de Jesus <miqueias.ufpi@gmail.com>
2025-08-25 17:08:41 -0300

    Create parse_quake_log.py
    Add script to parse log file

---
+++ parse_quake_log.py
@@ -0,0 +1,26 @@
+import re
+import json
+
+players = set()
+kills = {}
+
+with open('qgames.log', 'r') as f:
+    for line in f:
+        match = re.match(r'.*Kill:.*: (.*) killed (.*) by (.*)', line)
+        if match:
+            killer, victim, means = match.groups()
+            if killer != '<world>':
+                players.add(killer)
+                kills[killer] = kills.get(killer, 0) + 1
+            else:
+                # <world> kills: decrease victim's kill count
+                kills[victim] = kills.get(victim, 0) - 1
+            if victim != '<world>':
+                players.add(victim)
+
+output = {
+    "players": list(players),
+    "kills": {player: kills.get(player, 0) for player in players}
+}
+
+print(json.dumps(output, indent=2))
