5b7e87398068d315e6367ef8caad5d88917c466c
Miqueias Silva <miqueias.ufpi@gmail.com>
2025-08-25 17:20:54 -0300

    Update parser to correct expected output

--- parse_quake_log.py
+++ parse_quake_log.py
@@ -1,26 +1,29 @@
 import re
 import json
 
-players = set()
-kills = {}
+def parse_log(log_path):
+    players = set()
+    kills = {}
 
-with open('qgames.log', 'r') as f:
-    for line in f:
-        match = re.match(r'.*Kill:.*: (.*) killed (.*) by (.*)', line)
-        if match:
-            killer, victim, means = match.groups()
-            if killer != '<world>':
-                players.add(killer)
-                kills[killer] = kills.get(killer, 0) + 1
-            else:
-                # <world> kills: decrease victim's kill count
-                kills[victim] = kills.get(victim, 0) - 1
-            if victim != '<world>':
-                players.add(victim)
+    with open(log_path) as f:
+        for line in f:
+            match = re.match(r'.*Kill:.*: (.*) killed (.*) by (.*)', line)
+            if match:
+                killer, victim, means = match.groups()
+                if killer != '<world>':
+                    players.add(killer)
+                    kills[killer] = kills.get(killer, 0) + 1
+                else:
+                    # <world> kills: decrease victim's kill count
+                    kills[victim] = kills.get(victim, 0) - 1
+                if victim != '<world>':
+                    players.add(victim)
 
-output = {
-    "players": list(players),
-    "kills": {player: kills.get(player, 0) for player in players}
-}
+    return {
+        "players": list(players),
+        "kills": {player: kills.get(player, 0) for player in players}
+    }
 
-print(json.dumps(output, indent=2))
+if __name__ == "__main__":
+    result = parse_log("qgames.log")
+    print(json.dumps(result, indent=2))
